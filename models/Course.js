e53e256095fe5cd8af0c9a02318a48e3fd372a3e
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2020-12-19 21:58:04 +0200

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const courseSchema = mongoose.Schema({
     name : {
         type : String,
-        required : true
+        required : true,
+        unique : true
     },
     instructors : {
         type : []
