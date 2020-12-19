36f84c7bd74f160c39a7fab6fc38a9e75982f81e
FakhrTarabay <fakhretarabay@gmail.com>
2020-12-19 22:21:54 +0200

    2nd

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify',false);
+const express = require('express');
+const router = express.Router();
+const locM = require('../models/Location');
+const depM = require('../models/Department');
+const courseM = require('../models/Course');
+const facM = require('../models/Faculty');
+const schM = require('../models/Schedule');
+const staffM = require('../models/StaffMember');
+const reqM = require('../models/Request');
+
+router.route('/viewslots')
+.post(async(req,res)=>{
+    const slots = await schM.find({slot:1},{location:1},{course:1},{academicMember:req.body.name});
+    res.send(slots);
+})
+router.route('/viewStaffInDep')
+.post(async(req,res)=>{
+    const dep = await staffM.find({name:req.body.name}).select('department');
+    const staff = await staffM.find({},{department:dep});
+    res.send(staff);
+})
 const {app} = require('./app')
 require('dotenv').config()
 mongoose.connect(process.env.DB_URL,{useNewUrlParser: true, useUnifiedTopology: true}).then(console.log("pass"))
