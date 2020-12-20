a9a133b2dc6963f57f7c19a131f35c609b2ef0ca
Rawan Elkhouly <52850403+rawaann@users.noreply.github.com>
2020-12-20 09:47:16 -1200

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const login_route = require('./routes/login')
 const hr_routes = require('./routes/hr_routes')
+const blocklist = require('./models/tokens')
 const staff_routes = require('./routes/staff_routes')
 app.use(express.json());
 const jwt=require('jsonwebtoken')
@@ -11,8 +12,10 @@ app.use('', login_route)
 
 app.use(async(req,res,next)=>{
     const token=req.headers.token
+    const checkedToken = await blocklist.findOne({header: token})
     console.log(token);
-    if(token){
+    console.log(await blocklist.findOne({header: token}))
+    if(!checkedToken){
         const verified = jwt.verify(token,process.env.TOKEN_SECRET);
         console.log(verified);
         req.user=verified;
