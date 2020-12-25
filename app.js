58919c7191be27f21010d256120ac1fa4a3e45c9
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2020-12-25 23:29:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express=require('express');
 const app=express();
 const login_route = require('./routes/login')
-const hr_routes = require('./routes/hr_routes')
+const hr_routes = require('./routes/hr')
+const hod_routes = require('./routes/HOD')
+const blocklist = require('./models/tokens')
 const staff_routes = require('./routes/staff_routes')
+const coordinator_routes = require('./routes/coordinator')
+const academicMember_routes = require('./routes/AcademicMember')
+const instructor_routes = require('./routes/instructor')
 app.use(express.json());
 const jwt=require('jsonwebtoken')
 require('dotenv').config();
@@ -10,21 +15,31 @@ require('dotenv').config();
 app.use('', login_route)
 
 app.use(async(req,res,next)=>{
-    const token=req.headers.token
-    console.log(token);
-    if(token){
-        const verified = jwt.verify(token,process.env.TOKEN_SECRET);
-        console.log(verified);
-        req.user=verified;
+    try {
+        const token=req.headers.token
+        const checkedToken = await blocklist.findOne({header: token})
+        if(!checkedToken){
+            console.log(token)
+            const verified = jwt.verify(token,process.env.TOKEN_SECRET);
+            req.user=verified;
+        }
+        next();
+    } 
+    catch (error) {
+        res.send("Log in first!")    
     }
-    next();
 
 })
 app.use('', staff_routes)
 
-app.use('', hr_routes)
+app.use('/HR', hr_routes)
 
-//app.use()
+app.use('/HOD', hod_routes)
 
+app.use('/Coordinator', coordinator_routes)
+
+app.use('/AcademicMember', academicMember_routes)
+
+app.use('/Instructor', instructor_routes)
 
 module.exports.app=app; 
